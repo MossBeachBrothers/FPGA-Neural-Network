bf82aced6975b7f31d34aff0e6afbc4b91a71875
Akhilendra Nair <mossbeachbrothers@gmail.com>
2024-08-03 12:23:47 -0700

    test with bird image

--- visualize_output.py
+++ visualize_output.py
@@ -34,7 +34,7 @@ def show_images_stacked(image1, title1, image2, title2):
     plt.show()
 
 # Read the images
-gradient_image = read_image("test_gradients_720p.txt", 1280, 720)
+gradient_image = read_image("bird_rgb_values.txt", 1280, 720)
 output_image = read_image("output_720p.txt", 1280, 720)
 
 # Display the images stacked on top of each other
